efca296979444c1f8700ceb20ce0eec24c33abf1
biwnco102-a11y <biwnco102@gmail.com>
2026-09-23 01:23:34 +0700

    Add Light to Scene

--- Assets/Script/Destroy.cs
+++ Assets/Script/Destroy.cs
@@ -4,8 +4,25 @@ public class Destroy : MonoBehaviour
 {
     [SerializeField] private float delay = 0;
 
+    [Header("Destryo Chance")]
+    [SerializeField] bool isDestroyChance = false;
+    [SerializeField] float spawnChance = 100;
+    private float num = 0;
+
     private void Start()
     {
-        Destroy(gameObject, delay);
+        if (isDestroyChance)
+        {
+            num = Random.Range(0, 101);
+        }
+
+        if (num <= spawnChance)
+        {
+            Destroy(gameObject, delay);
+        }
+        else if (num >= spawnChance)
+        {
+            Destroy(this);
+        }
     }
 }
